8eff7899839074dfea1a2be85b5fd68ffa238897
youssef hafnawi <77534098+ymhaah@users.noreply.github.com>
2023-01-03 22:39:03 +0200

    Detect Capital: first thoughts

---
+++ index.js
@@ -0,0 +1,26 @@
+function log(ele) {
+	console.log(ele);
+}
+
+// ####################################################
+// detect-capital
+
+// link: https://leetcode.com/problems/detect-capital/
+
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+
+let word1 = "USA";
+let word2 = "egypT";
+let word3 = "Yousef";
+
+function isUpperCase(word) {
+	let arr = word.split("", word.length);
+}
+isUpperCase(word1);
+
+// ####################################################
