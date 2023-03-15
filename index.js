4792cd4993ba881b864b9dc98a765c9dc81eee8b
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2023-03-15 13:07:42 +0200

    Equalize Frequency #1

--- index.js
+++ index.js
@@ -120,3 +120,72 @@
 //     return false;
 // }
 // console.log(isPalindrome(x));
+
+// ####################################################
+
+// Remove Letter To Equalize Frequency
+// link: https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+
+// You are given a 0-indexed string word, consisting of lowercase English letters.
+//  You need to select one index and remove the letter at that index from word so that the frequency of every letter present in word is equal.
+
+// Return true if it is possible to remove one letter so that the frequency of all letters in word are equal, and false otherwise.
+
+// Note:
+
+// The frequency of a letter x is the number of times it occurs in the string.
+// You must remove exactly one letter and cannot chose to do nothing.
+
+// Example 1:
+
+// Input: word = "abcc"
+// Output: true
+// Explanation: Select index 3 and delete it: word becomes "abc" and each character has a frequency of 1.
+// Example 2:
+
+// Input: word = "aazz"
+// Output: false
+// Explanation: We must delete a character, so either the frequency of "a" is 1 and the frequency of "z" is 2, or vice versa.
+//  It is impossible to make all present letters have equal frequency.
+
+let word1 = "aabcc";
+let word2 = "ccccaa";
+
+function equalFrequency(word) {
+    // if (word.split("").length === 0) return false;
+
+    // let arr = [];
+    // let num = 0;
+    // for (let i = 0; i < word.split("").length; i++) {
+    //     let cur = word.split("")[i];
+    //     for (let j = i + 1; j < word.split("").length; j++) {
+    //         if (cur === word.split("")[j] && i != j) {
+    //             arr.push(cur);
+
+    //             break;
+    //         }
+    //     }
+    // }
+    // console.log("arr:", arr);
+
+    // let freqStr = [];
+    // for (let i = 0; i < word.split("").length; i++) {
+    //     let cur = word.split("")[i];
+    //     for (let j = 0; j < word.split("").length; j++) {
+    //         let num = 0;
+    //         if (cur === word.split("")[j] && i != j) {
+    //             freqStr.push(cur);
+    //             num++;
+    //             if (num > 1) {
+    //                 let freqStrSet = new Set(freqStr);
+    //                 if (freqStrSet.size > 1) {
+    //                     return false;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    return true;
+}
+// equalFrequency(word1)
+console.log("equalFrequency", equalFrequency(word2));
